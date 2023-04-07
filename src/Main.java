974d6b447d585aa7ac554d8e4d8ed0739450afbd
rogowska <rogowska.oliwia@gmail.com>
2023-04-07 22:01:05 +0200

    changing structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree newTree = new Tree(1, 10);
+        newTree.insert(5);
+        newTree.insert(4);
+        newTree.insert(8);
+        newTree.insert(5);
+        newTree.insert(9, 0);
+        newTree.print();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,13 +1,57 @@
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Tree {
-    private Node root;
+
+    private ArrayList<Integer> tree;
     private int size;
 
-    public int getSize(){
-        return size;
-    }
 
-    public Tree(int value){
-        this.root.value = value;
+    public Tree(int value, int capacity){
+        tree = new ArrayList<Integer>(capacity);
+        this.tree.add(0, value);
         this.size = 1;
     }
+
+    public void insert(int value, int parentIndex){
+        insert(value);
+        int currentIndex = size - 1;
+        int secondIndex;
+        while ((findParentIndex(currentIndex) != parentIndex) && currentIndex != 0){
+            secondIndex = currentIndex;
+            currentIndex = findParentIndex(currentIndex);
+            Collections.swap(this.tree, currentIndex, secondIndex);
+        }
+
+    }
+
+     public void insert(int value){
+        this.tree.add(size, value);
+        this.size++;
+
+     }
+
+     public void print(){
+        for(Integer x: this.tree){
+            System.out.println(x);
+        }
+     }
+
+     private int findParentIndex(int i){
+         return (i - 1) / 2;
+     }
+
+     private int findLeftChildIndex(int i){
+        return 0;
+     }
+
+     private int findRightChildIndex(int i){
+        return 0;
+     }
+
+
+
 }
